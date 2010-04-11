114485c227b8a5321cecfcd897a5bab5c2848621
Andrey Tarantsov <andreyvit@gmail.com>
2010-04-12 00:08:08 +0700

    Add sign in / sign out buttons.

--- web/makeapp/handlers.py
+++ web/makeapp/handlers.py
@@ -1,5 +1,5 @@
 
-from tipfy import RequestHandler, url_for, redirect, redirect_to
+from tipfy import RequestHandler, url_for, redirect, redirect_to, render_json_response
 from tipfy.ext.jinja2 import render_response
 
 from google.appengine.api import users
@@ -18,3 +18,18 @@ class DesignerHandler(RequestHandler):
     def get(self, **kwargs):
         context = {}
         return render_response('designer.html', **context)
+        
+class GetUserDataHandler(RequestHandler):
+    
+    def get(self, **kwargs):
+        user = users.get_current_user()
+        if user is None:
+            return render_json_response({
+                'status': 'anonymous',
+                'login_url': users.create_login_url(url_for('designer')),
+            })
+        else:
+            return render_json_response({
+                'status': 'authenticated',
+                'logout_url': users.create_logout_url(url_for('home')),
+            })
--- web/makeapp/urls.py
+++ web/makeapp/urls.py
@@ -5,4 +5,5 @@ def get_rules():
     return [
         Rule('/', endpoint='home', handler='makeapp.handlers:HomeHandler'),
         Rule('/designer/', endpoint='designer', handler='makeapp.handlers:DesignerHandler'),
+        Rule('/user-info.json', endpoint='api/get-user-data', handler='makeapp.handlers:GetUserDataHandler'),
     ]
