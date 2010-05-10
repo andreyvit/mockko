d6c6930fbf1c25511d81712554874161c9590137
Andrey Tarantsov <andreyvit@gmail.com>
2010-05-10 20:12:10 +0700

    Fix for serving processed images to anonymous users.

--- web/makeapp/handlers.py
+++ web/makeapp/handlers.py
@@ -147,72 +147,71 @@ class ServeImageHandler(RequestHandler):
 class ServeProcessedImageHandler(RequestHandler):
 
     def get(self, image_name, effect):
-        user = users.get_current_user()
-        if user is None:
-            return render_json_response({ 'error': 'signed-out' })
-        else:
-            if effect in ('iphone-tabbar-active', 'iphone-tabbar-inactive'):
-                if image_name.startswith('stock--'):
-                    path = os.path.join(os.path.dirname(__file__), '..', 'server-images', image_name.replace('--', '/'))
-                    kw = dict(file=open(path, 'rb'))
-                else:
-                    account = Account.all().filter('user', user).get()
-                    if account is None:
-                        raise NotFound()
+        if effect in ('iphone-tabbar-active', 'iphone-tabbar-inactive'):
+            if image_name.startswith('stock--'):
+                path = os.path.join(os.path.dirname(__file__), '..', 'server-images', image_name.replace('--', '/'))
+                kw = dict(file=open(path, 'rb'))
+            else:
+                user = users.get_current_user()
+                if user is None:
+                    return render_json_response({ 'error': 'signed-out' })
+                account = Account.all().filter('user', user).get()
+                if account is None:
+                    raise NotFound()
 
-                    img = Image.get_by_key_name(image_name)
-                    if img is None or img.account.key() != account.key():
-                        raise NotFound()
-                
-                    img_data = ImageData.get_by_key_name(image_name)
-                    if img_data is None:
-                        raise NotFound()
-                    
-                    kw = dict(byte=img_data.data)
+                img = Image.get_by_key_name(image_name)
+                if img is None or img.account.key() != account.key():
+                    raise NotFound()
+    
+                img_data = ImageData.get_by_key_name(image_name)
+                if img_data is None:
+                    raise NotFound()
+        
+                kw = dict(byte=img_data.data)
 
-                square_file = os.path.join(os.path.dirname(__file__), '..', 'server-images', effect + '.png')
-                sw, sh, spix, smeta = png.Reader(file=open(square_file, 'rb')).asRGBA8()
-                uw, uh, upix, umeta = png.Reader(**kw).asRGBA8()
+            square_file = os.path.join(os.path.dirname(__file__), '..', 'server-images', effect + '.png')
+            sw, sh, spix, smeta = png.Reader(file=open(square_file, 'rb')).asRGBA8()
+            uw, uh, upix, umeta = png.Reader(**kw).asRGBA8()
+    
+            spix = list(spix)
+            upix = list(upix)
+    
+            # raise StandardError, repr(spix)
+    
+            wins = (sw-uw) / 2
+            hins = (sh-uh) / 2
+    
+            res = []
+    
+            sy = 0
+            upix = iter(upix)
+            for srow in iter(spix):
+                srow = iter(srow)
+                uy = sy - hins
+                urow = (iter(upix.next()) if uy >= 0 and uy < uh else None)
+                resrow = []
+                res.append(resrow)
+                sx = 0
+                while sx < sw:
+                    s_r, s_g, s_b, s_a = srow.next(), srow.next(), srow.next(), srow.next()
             
-                spix = list(spix)
-                upix = list(upix)
+                    ux = sx - wins
+                    if urow is not None and ux >= 0 and ux < uw:
+                        u_r, u_g, u_b, u_a = urow.next(), urow.next(), urow.next(), urow.next()
+                    else:
+                        u_a = 0
             
-                # raise StandardError, repr(spix)
-            
-                wins = (sw-uw) / 2
-                hins = (sh-uh) / 2
-            
-                res = []
-            
-                sy = 0
-                upix = iter(upix)
-                for srow in iter(spix):
-                    srow = iter(srow)
-                    uy = sy - hins
-                    urow = (iter(upix.next()) if uy >= 0 and uy < uh else None)
-                    resrow = []
-                    res.append(resrow)
-                    sx = 0
-                    while sx < sw:
-                        s_r, s_g, s_b, s_a = srow.next(), srow.next(), srow.next(), srow.next()
-                    
-                        ux = sx - wins
-                        if urow is not None and ux >= 0 and ux < uw:
-                            u_r, u_g, u_b, u_a = urow.next(), urow.next(), urow.next(), urow.next()
-                        else:
-                            u_a = 0
-                    
-                        resrow.append(s_r)
-                        resrow.append(s_g)
-                        resrow.append(s_b)
-                        resrow.append(u_a)
-                        sx += 1
-            
-                    sy += 1
-            
-                f = StringIO()
-                png.Writer(sw, sh, bitdepth=8, alpha=True, planes=4).write(f, res)
-                return Response(response=f.getvalue(), mimetype='image/png')
+                    resrow.append(s_r)
+                    resrow.append(s_g)
+                    resrow.append(s_b)
+                    resrow.append(u_a)
+                    sx += 1
+    
+                sy += 1
+    
+            f = StringIO()
+            png.Writer(sw, sh, bitdepth=8, alpha=True, planes=4).write(f, res)
+            return Response(response=f.getvalue(), mimetype='image/png')
 
 class DeleteImageHandler(RequestHandler):
 
