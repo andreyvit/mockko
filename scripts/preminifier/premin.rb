b04fa1d5fe889d78d2b9559be59cfb33ebd80a72
Andrey Tarantsov <andreyvit@gmail.com>
2010-06-16 09:58:19 +0700

    Fix: Cannot upload images in compiled version

--- scripts/preminifier/premin.rb
+++ scripts/preminifier/premin.rb
@@ -489,7 +489,7 @@ STOPLISTS = {
       text toggle toggleClass trigger triggerHandler unbind unload val width wrap wrapAll wrapInner/,
     %w/$.fn $.fn.showTags jQuery.fn jQuery.fn.showTags/,
     %w/closest success/,
-    %w/sortable has webkit/],
+    %w/sortable has webkit processData beforeSend contentType dataType/],
   :undescore => %w/_ each map reduce reduceRight detect select reject all any include invoke
       pluck max min sortBy sortedIndex toArray size
       first rest last compact flatten without uniq intersect zip indexOf lastIndexOf range
