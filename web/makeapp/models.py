05440fda44cdd9a3904d9de2a24999a7a8b5dc8c
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-06-18 03:21:36 +0700

    Trim trailing whitespace

--- web/makeapp/models.py
+++ web/makeapp/models.py
@@ -7,16 +7,16 @@ from google.appengine.ext import db
 
 class Account(db.Model):
     user = db.UserProperty()
-    
+
     invited_by = db.SelfReferenceProperty()
-    
+
     created_at = db.DateTimeProperty(auto_now_add=True)
     updated_at = db.DateTimeProperty(auto_now=True)
-    
+
 class App(db.Model):
     name = db.StringProperty(required=True)
     body = db.TextProperty()
-    
+
     created_by = db.ReferenceProperty(Account)
     editors    = db.ListProperty(db.Key)
 
