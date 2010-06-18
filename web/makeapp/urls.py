62bb261054fc78bfb5652c927650f000852ad3bf
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-06-18 18:28:10 +0700

    Make sure / and /user-info.json are only GETed

--- web/makeapp/urls.py
+++ web/makeapp/urls.py
@@ -2,8 +2,9 @@ from tipfy import Rule
 
 def get_rules():
     return [
-        Rule('/', endpoint='home', handler='makeapp.handlers:HomeHandler'),
-        Rule('/user-info.json', endpoint='api/get-user-data', handler='makeapp.handlers:GetUserDataHandler'),
+        Rule('/', endpoint='home', methods=['GET'], handler='makeapp.handlers:HomeHandler'),
+        Rule('/user-info.json', endpoint='api/get-user-data', methods=['GET'], handler='makeapp.handlers:GetUserDataHandler'),
+
         Rule('/apps/', endpoint='api/get-app-list', methods=['GET'], handler='makeapp.handlers:GetAppListHandler'),
         Rule('/apps/', endpoint='api/save-new-app', methods=['POST'], handler='makeapp.handlers:SaveAppHandler'),
         Rule('/apps/<int:app_id>/', endpoint='api/save-app', methods=['POST'], handler='makeapp.handlers:SaveAppHandler'),
