aae2e9d073b084576c793f3d43678b6b2a236ece
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-06-18 18:28:25 +0700

    Reorder rules a bit

--- web/makeapp/urls.py
+++ web/makeapp/urls.py
@@ -6,13 +6,16 @@ def get_rules():
         Rule('/user-info.json', endpoint='api/get-user-data', methods=['GET'], handler='makeapp.handlers:GetUserDataHandler'),
 
         Rule('/apps/', endpoint='api/get-app-list', methods=['GET'], handler='makeapp.handlers:GetAppListHandler'),
+        Rule('/R<int:app_id>/', endpoint='api/run-app', methods=['GET'], handler='makeapp.handlers:RunAppHandler'),
+
         Rule('/apps/', endpoint='api/save-new-app', methods=['POST'], handler='makeapp.handlers:SaveAppHandler'),
         Rule('/apps/<int:app_id>/', endpoint='api/save-app', methods=['POST'], handler='makeapp.handlers:SaveAppHandler'),
         Rule('/apps/<int:app_id>/', endpoint='api/delete-app', methods=['DELETE'], handler='makeapp.handlers:DeleteAppHandler'),
-        Rule('/R<int:app_id>/', endpoint='api/run-app', methods=['GET'], handler='makeapp.handlers:RunAppHandler'),
+
         Rule('/images/', endpoint='api/get-image-list', methods=['GET'], handler='makeapp.handlers:GetImageListHandler'),
-        Rule('/images/', endpoint='api/save-image', methods=['POST'], handler='makeapp.handlers:SaveImageHandler'),
         Rule('/images/<image_name>', endpoint='api/serve-image', methods=['GET'], handler='makeapp.handlers:ServeImageHandler'),
         Rule('/images/<image_name>/<effect>', endpoint='api/serve-processed-image', methods=['GET'], handler='makeapp.handlers:ServeProcessedImageHandler'),
+
+        Rule('/images/', endpoint='api/save-image', methods=['POST'], handler='makeapp.handlers:SaveImageHandler'),
         Rule('/images/<image_name>', endpoint='api/delete-image', methods=['DELETE'], handler='makeapp.handlers:DeleteImageHandler'),
     ]
